cdbd4b1bc90972cc606620ba2df47002f1bba854
Clayton Watterson <cwatterson@gmail.com>
2022-12-07 09:19:47 -0500

    Day 06 - Problem 01

---
+++ 2022/Day06/day06.js
@@ -0,0 +1,35 @@
+const {readFileSync} = require('fs');
+
+let problemOne = () => {
+
+  let data = readFileSync('./Day06/input.txt', 'utf-8').split(/\n/);
+  data = data[0].split('');
+
+  let front = 1;
+  let back = 0;
+  let code = [data[back]];
+
+  while (front - back < 4) {
+    if (code.includes(data[front])) {
+      while (code.includes(data[front])) {
+        code.shift();
+        back++;
+      }
+    }
+    code.push(data[front]);
+    front++;
+  }
+
+  return front;
+
+}
+
+let problemTwo = () => {
+
+  const data = readFileSync('./Day06/input.txt', 'utf-8').split(/\n/);
+
+  return null;
+
+}
+
+module.exports = {problemOne, problemTwo};
---
+++ 2022/Day06/day06.test.js
@@ -0,0 +1,9 @@
+const { problemOne, problemTwo } = require('./day06.js');
+
+test('Day06_Problem01', () => {
+  expect(problemOne()).toStrictEqual(1356);
+});
+
+test('Day06_Problem02', () => {
+  expect(problemTwo()).toStrictEqual(null);
+});
