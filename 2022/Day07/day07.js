c55aacba0f16160cdd48047cf5760bc67927e934
Clayton Watterson <cwatterson@gmail.com>
2022-12-07 13:12:53 -0500

    setup Day07

---
+++ 2022/Day07/day07.js
@@ -0,0 +1,19 @@
+const {readFileSync} = require('fs');
+
+let problemOne = () => {
+
+  let data = readFileSync('./Day07/input.txt', 'utf-8').split(/\n/);
+
+  return null;
+
+}
+
+let problemTwo = () => {
+
+  let data = readFileSync('./Day07/input.txt', 'utf-8').split(/\n/);
+
+  return null;
+
+}
+
+module.exports = {problemOne, problemTwo};
---
+++ 2022/Day07/day07.test.js
@@ -0,0 +1,9 @@
+const { problemOne, problemTwo } = require('./day07.js');
+
+test('Day07_Problem01', () => {
+  expect(problemOne()).toStrictEqual(null);
+});
+
+test('Day07_Problem02', () => {
+  expect(problemTwo()).toStrictEqual(null);
+});
