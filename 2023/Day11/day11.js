4c8357ffa76494ab508772a8c5710ea13e48e479
Clayton Watterson <cwatterson@gmail.com>
2023-12-12 11:00:21 -0500

    Day 11 Part 02

--- 2023/Day11/day11.js
+++ 2023/Day11/day11.js
@@ -54,8 +54,75 @@ let problemOne = () => {
 
 
 let problemTwo = () => {
-    let data = readFileSync('./Day11/input.txt', 'utf-8').split(/\n/);
-    return -1;
+    const data = readFileSync('./Day11/input.txt', 'utf-8').split(/\r\n/);
+    const universe = [];
+    for (const element of data) {
+        universe.push(element.split(''));
+    }
+    const expandedHorizUniverse = [];
+    const columnsExpanded = new Set();
+    const rowsExpanded = new Set();
+
+    for (let row of universe) {
+        expandedHorizUniverse.push(row);
+        if (!row.includes('#')) {
+            const newRow = [];
+            for (let i = 0; i < row.length; i++) {
+                newRow.push('@');
+            }
+            expandedHorizUniverse.push(newRow);
+            rowsExpanded.add(expandedHorizUniverse.length - 1);
+        }
+    }
+    const expandedUniverse = [];
+    for (let i = 0; i < expandedHorizUniverse.length; i++) {
+        expandedUniverse.push([]);
+    }
+    for (let col = 0; col < expandedHorizUniverse[0].length; col++) {
+        let containsHash = false;
+        for (let row = 0; row < expandedHorizUniverse.length; row++) {
+            expandedUniverse[row].push(expandedHorizUniverse[row][col]);
+            if (expandedHorizUniverse[row][col] === '#') {
+                containsHash = true;
+            }
+        }
+        if (!containsHash) {
+            for (let row = 0; row < expandedHorizUniverse.length; row++) {
+                expandedUniverse[row].push('@');
+            }
+            columnsExpanded.add(expandedUniverse[0].length - 1);
+        }
+    }
+
+    const hashLocations = [];
+    for (let row = 0; row < expandedUniverse.length; row++) {
+        for (let col = 0; col < expandedUniverse[0].length; col++) {
+            if (expandedUniverse[row][col] === '#') {
+                hashLocations.push({row, col})
+            }
+        }
+    }
+
+    let totalDistance = 0;
+    for (let i = 0; i < hashLocations.length - 1; i++) {
+        for (let j = i + 1; j < hashLocations.length; j++) {
+            let numExpandedRowsToCross = 0;
+            for (let k = Math.min(hashLocations[i].row, hashLocations[j].row); k < Math.max(hashLocations[i].row, hashLocations[j].row); k++) {
+                if (rowsExpanded.has(k)) {
+                    numExpandedRowsToCross++;
+                }
+            }
+            let numExpandedColsToCross = 0;
+            for (let k = Math.min(hashLocations[i].col, hashLocations[j].col); k < Math.max(hashLocations[i].col, hashLocations[j].col); k++) {
+                if (columnsExpanded.has(k)) {
+                    numExpandedColsToCross++;
+                }
+            }
+            totalDistance += Math.abs(hashLocations[i].row - hashLocations[j].row) - numExpandedRowsToCross * 2 + Math.abs(hashLocations[i].col - hashLocations[j].col) - numExpandedColsToCross * 2 + (numExpandedRowsToCross * 1000000) + (numExpandedColsToCross * 1000000);
+        }
+    }
+
+    return totalDistance;
 }
 
 module.exports = {problemOne, problemTwo};
--- 2023/Day11/day11.test.js
+++ 2023/Day11/day11.test.js
@@ -1,9 +1,9 @@
 const {problemOne, problemTwo} = require('./day11.js');
 
-test('Day11_Problem01', () => {
+test.skip('Day11_Problem01', () => {
     expect(problemOne()).toStrictEqual(10494813);
 });
 
-test.skip('Day11_Problem02', () => {
-    expect(problemTwo()).toStrictEqual(0);
+test('Day11_Problem02', () => {
+    expect(problemTwo()).toStrictEqual(840988812853);
 });
